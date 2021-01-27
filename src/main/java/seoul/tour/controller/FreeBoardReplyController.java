e947d39071b229c94db9278d60fd33fcd0da348d
admin1 <leski@naver.com>
2021-01-27 23:21:19 +0900

    댓글 작업 컨트롤러 작성함

--- src/main/java/seoul/tour/controller/FreeBoardReplyController.java
+++ src/main/java/seoul/tour/controller/FreeBoardReplyController.java
@@ -5,17 +5,20 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import seoul.tour.domain.Criteria;
 import seoul.tour.domain.FreeBoardReplyVO;
+import seoul.tour.domain.FreeBoardVO;
 import seoul.tour.service.FreeBoardReplyService;
 
 @RequestMapping("/freeboardreplies/")
@@ -59,5 +62,47 @@ public class FreeBoardReplyController {
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
 	}
 	
-	//397p		
+	// ¥Ò±€ ªË¡¶
+	@GetMapping(value = "/{rno}",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<FreeBoardReplyVO> get(@PathVariable("rno") Long rno) {
+		
+		log.info("get: " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	//¥Ò±€ ªË¡¶
+	@DeleteMapping(value= "/{rno}" , produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		
+		log.info("remove: " + rno) ;
+		
+		return service.remove(rno) == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	//¥Ò±€ ºˆ¡§
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH} ,
+			value = "/{rno}",
+			consumes = "application/json",
+			produces = {MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(
+			@RequestBody FreeBoardReplyVO vo,
+			@PathVariable("rno") Long rno) {
+		
+		vo.setRno(rno);
+		
+		log.info("rno: " + rno);
+		log.info("modify:" + vo);
+		
+		return service.modify(vo) == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
 }
